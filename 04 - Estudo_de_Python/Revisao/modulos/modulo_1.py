2549aa3c77b2d584a72741cd5dae0caac3095b58
Ingrid Santana <is.matos@outlook.com>
2024-11-12 11:59:48 -0300

    modulos

---
+++ 04 - Estudo_de_Python/Revisao/modulos/modulo_1.py
@@ -0,0 +1,7 @@
+def saudacao(nome):
+    """
+    A função saudação exibe uma mensagem de recepção.
+    ele pede um parâmetro(argumento) chamado nome.
+    Você deve passar o nome de alguém
+    """
+    print(f"Olá, {nome}!")
---
+++ 04 - Estudo_de_Python/Revisao/modulos/usar_modulo_1.py
@@ -0,0 +1,4 @@
+import modulo_1 
+
+modulo_1.saudacao("Roberta")
+modulo_1.saudacao("Joana")
